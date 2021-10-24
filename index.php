c2df948c6e163abd78820883cb760d677eb861ab
domo1819 <85992996+domo1819@users.noreply.github.com>
2021-10-24 22:47:28 +0900

    Update index.php

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
 					}
 					pg_set_client_encoding("UTF-8");
 
-					$result = pg_query($conn, "select id,user_id,timestamp from warn_info"); 
+					$result = pg_query($conn, "select id,user_id,timestamp,car_data_id from warn_info"); 
 
 
 					//stringの配列情報
