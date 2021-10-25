9166d31d7a0c249a858d6a6e72bcd97b1f57424c
domo1819 <85992996+domo1819@users.noreply.github.com>
2021-10-26 00:21:06 +0900

    Update index.php

--- index.php
+++ index.php
@@ -76,7 +76,7 @@
 					}
 					pg_set_client_encoding("UTF-8");
 
-					$result = pg_query($conn, "SELECT region_data.id, region_data.region_name, car_data.car_region_id, car_data.car_classify_num, car_data.car_classify_hiragana, car_data.car_number,  warn_info.timestamp, warn_info.punish_id, warn_info.is_payment FROM region_data, car_data, warn_info WHERE region_name LIKE '%{$_POST['word']}%' OR car_number LIKE '%{$_POST['word']}%'"); 
+					$result = pg_query($conn, "SELECT  region_data.region_name, car_data.car_region_id, car_data.car_classify_num, car_data.car_classify_hiragana, car_data.car_number, warn_info.id, warn_info.timestamp, warn_info.punish_id, warn_info.is_payment FROM region_data, car_data, warn_info WHERE region_name LIKE '%{$_POST['word']}%' OR car_number LIKE '%{$_POST['word']}%'"); 
 
 
 					//stringの配列情報
