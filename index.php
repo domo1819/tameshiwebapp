faa1dae5f48a1fe4bae53d3fbe4d84a1c93118d4
domo1819 <85992996+domo1819@users.noreply.github.com>
2021-10-26 00:51:03 +0900

    Update index.php

--- index.php
+++ index.php
@@ -76,7 +76,7 @@
 					}
 					pg_set_client_encoding("UTF-8");
 
-					$result = pg_query($conn, "SELECT warn_info.user_id, warn_info.user_id, warn_info.timestamp, warn_info.punish_id,  warn_info.is_payment FROM warn_info"); 
+					$result = pg_query($conn, "SELECT warn_info.id, warn_info.user_id, warn_info.timestamp, warn_info.punish_id,  warn_info.is_payment FROM warn_info"); 
 
 
 					//stringの配列情報
