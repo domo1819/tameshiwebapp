93cbc96bc1edfee20a9f7eba3c9f69e7d4f78154
domo1819 <85992996+domo1819@users.noreply.github.com>
2021-10-29 17:03:07 +0900

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,32 @@
 		検索サイト
 	</title>
     <script type="text/javascript" src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+		<script type="text/javascript">
+        $(function(){
+	//.sampleをクリックしてajax通信を行う
+	$('.search_button').click(function(){
+		var data = {request : $('#request').val()};
+			$.ajax({
+					url: 'index.php',
+					type: 'POST',
+					/* json形式で受け取るためdataTypeを変更 */
+					data: data,
+
+					//処理が成功したら
+					success : function(data, dataType) {
+						//HTMLファイル内の該当箇所にレスポンスデータを追加します。
+						$('#res').html(data);
+				},
+				//処理がエラーであれば
+				error : function() {
+						alert('通信エラー');
+				}
+ });
+ //submitによる画面リロードを防いでいます。
+ return false;
+});
+});
+	</script>
 	<link rel="stylesheet" href="../css/style.css">
 </head>
 <body><!--  人間が見る内容を記述 -->
