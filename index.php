655890cc3de1164d6690684acb1ae029d860d6bc
domo1819 <85992996+domo1819@users.noreply.github.com>
2021-10-29 21:51:40 +0900

    Update index.php

--- index.php
+++ index.php
@@ -62,7 +62,7 @@
     				</table>
 								<?php
 								  $con = pg_connect(getenv("DATABASE_URL"));
-										$co = pg_query($con, "SELECT id,timestamp,region_name,car_classify_num,car_classify_hiragana,car_number,fine_amount,afk_mode FROM warn_info a INNER JOIN car_data b ON a.car_data_id=b.id INNER JOIN region_data c ON b.car_region_id=c.id INNER JOIN punish_data d ON a.punish_id=d.id INNER JOIN fine_data e ON d.fine_id=e.id INNER JOIN afk_mode_data f ON d.afk_mode_id=f.id INNER JOIN user_data g ON a.user_id=g.user_id INNER JOIN belong_data h ON g.belong_id = h.id");
+										$co = pg_query($con, "SELECT a.id, a.timestamp, h.belong_name, c.region_name, b.car_classify_num, b.car_classify_hiragana, b.car_number, e.fine_amount, f.afk_mode, a.is_payment FROM warn_info a INNER JOIN car_data b ON a.car_data_id=b.id INNER JOIN region_data c ON b.car_region_id=c.id INNER JOIN punish_data d ON a.punish_id=d.id INNER JOIN fine_data e ON d.fine_id=e.id INNER JOIN afk_mode_data f ON d.afk_mode_id=f.id INNER JOIN user_data g ON a.user_id=g.user_id INNER JOIN belong_data h ON g.belong_id = h.id");
 										$productList = array();
 										while($date = pg_fetch_array($co)){
 											$productList[] = array(
