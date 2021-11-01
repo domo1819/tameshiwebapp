e097897e806a1f7255d4d1134b81c2fbbfb77c8a
domo1819 <85992996+domo1819@users.noreply.github.com>
2021-11-01 16:57:41 +0900

    Update index.php

--- index.php
+++ index.php
@@ -41,34 +41,11 @@
 				<form method="POST" action="index.php">
 					<div class="engine2">
 						<label>検索項目</label>
-						<select name="dealer" id="dealer">
-							<option value="dealerA">全件検索</option>
-							<option value="dealerB">日付検索</option>
-							<option value="dealerC">日時</option>
+						<select name="emp">
+							<option value="all"<?php if ($emp === 'all'){ print 'selected'; } ?>>全件検索</option>
+							<option value="つくば"<?php if ($emp === 'つくば') { print 'selected'; } ?>>日付検索</option>
+							<option value="越谷"<?php if ($emp === '越谷') { print 'selected'; } ?>>日時</option>
 						</select><br><br>
-						<select>
-						<?php
-								$con = pg_connect(getenv("DATABASE_URL"));
-								if (!$con)  {
-									exit('データベースに接続できませんでした。');
-								}
-									$col = pg_query($con, "SELECT region_name FROM region_data ORDER BY region_name;");
-									while($data = pg_fetch_array($col)){
-									?>
-									<OPTION VALUE="<?php $data['region_name'] ?>"><?php echo $data['region_name'] ?></OPTION><?php
-									}
-									?>
-								</select><br><br>	
-								<label>日時検索</label>
-								<select>
-								<?php
-										$co = pg_query($con, "SELECT timestamp FROM warn_info ORDER BY timestamp;");
-										while($date = pg_fetch_array($co)){
-											?>
-											<OPTION VALUE="<?php $date['timestamp'] ?>"><?php echo $date['timestamp'] ?></OPTION><?php
-											}
-										?>
-								</select><br><br>	
 								<label>検索単語を入力してください。(空欄の場合は全検索をします。)</label>
 								<input type="text" id="search_text" name="word" placeholder="検索語を入力してください">
 								<br><br><br>
@@ -78,6 +55,67 @@
 					</div>
 				</form>
 			</div>
+			<?php
+				$emp = '';
+				$all = '';
+				if (isset($_POST['all']) === TRUE) {
+						$all = $_POST['all'];
+				}
+				if (isset($_POST['emp']) === TRUE) {
+						$emp = $_POST['emp'];
+				}
+				$emp_data = array();
+				$order = '';
+
+				$conn = pg_connect(getenv("DATABASE_URL"));
+					
+				// 接続成功した場合
+				if ($link) {
+					// 文字化け防止
+					pg_set_client_encoding("UTF-8");
+
+					if (isset($_POST['all']) === TRUE) {
+						$result = pg_query($conn, "SELECT a.id, a.timestamp, h.belong_name, c.region_name, b.car_classify_num, b.car_classify_hiragana, b.car_number, e.fine_amount, f.afk_mode, a.is_payment FROM warn_info a INNER JOIN car_data b ON a.car_data_id=b.id INNER JOIN region_data c ON b.car_region_id=c.id INNER JOIN punish_data d ON a.punish_id=d.id INNER JOIN fine_data e ON d.fine_id=e.id INNER JOIN afk_mode_data f ON d.afk_mode_id=f.id INNER JOIN user_data g ON a.user_id=g.user_id INNER JOIN belong_data h ON g.belong_id = h.id ORDER BY a.id ASC"); 
+					}
+
+					if (isset($_POST['emp']) === TRUE) {
+						$result = pg_query($conn, "SELECT * FROM region_data WHERE region_name = '$emp'");
+					} else {
+						$result = pg_query($conn, "SELECT a.id, a.timestamp, h.belong_name, c.region_name, b.car_classify_num, b.car_classify_hiragana, b.car_number, e.fine_amount, f.afk_mode, a.is_payment FROM warn_info a INNER JOIN car_data b ON a.car_data_id=b.id INNER JOIN region_data c ON b.car_region_id=c.id INNER JOIN punish_data d ON a.punish_id=d.id INNER JOIN fine_data e ON d.fine_id=e.id INNER JOIN afk_mode_data f ON d.afk_mode_id=f.id INNER JOIN user_data g ON a.user_id=g.user_id INNER JOIN belong_data h ON g.belong_id = h.id ORDER BY a.id ASC"); 
+					}
+					// 1行ずつ結果を配列で取得します
+					while ($row = pg_fetch_array($result)) {
+							$emp_data[] = $row;
+					}
+					// 結果セットを開放します
+					pg_free_result($result);
+					// 接続を閉じます
+					pg_close($conn);
+				// 接続失敗した場合
+				} else {
+					print 'DB接続失敗';
+				}
+
+				?>
+			<table border=1>
+       <tr>
+			 <th>ID</th>
+			 <th>日時</th>
+			 <th>所属名</th>
+			 <th>地域名</th>
+			 <th>分類番号(番号)</th>
+			 <th>分類番号(ひらがな)</th>
+			 <th>車番号</th>
+			 <th>罰金額</th>
+			 <th>違反態様</th>
+			 <th>支払い状況</th>
+       </tr>
+			 <?php
+			 foreach ($emp_data as $value) {
+				printf("<td>" .$value. "</td>\n");
+			 }
+			 ?>
+   </table>
 			<div id="dealerA">
 			<?php
 					$conn = pg_connect(getenv("DATABASE_URL"));
