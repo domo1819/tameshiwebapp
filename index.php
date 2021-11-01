25b3bea596a847e724a3f5002206a435270f2342
domo1819 <85992996+domo1819@users.noreply.github.com>
2021-11-01 19:14:20 +0900

    Update index.php

--- index.php
+++ index.php
@@ -43,15 +43,15 @@
 						<label>地域検索項目</label>
 						<select name="emp">
 							<option value="all">全件検索</option>
+							<option VALUE="<?php $data['region_name'] ?>">
+						  </option>	
 							<?php
 							    $conn = pg_connect(getenv("DATABASE_URL"));
 									$col = pg_query($conn, "SELECT region_name FROM region_data ORDER BY region_name;");
 									while($data = pg_fetch_array($col)){
-									?>
-									<OPTION VALUE="<?php $data['region_name'] ?>"><?php echo $data['region_name'] ?></OPTION><?php
 									}
 									pg_close($conn);
-									?>
+								?>
 						</select><br><br>
 								<label>検索単語を入力してください。(空欄の場合は全検索をします。)</label>
 								<input type="text" id="search_text" name="word" placeholder="検索語を入力してください">
