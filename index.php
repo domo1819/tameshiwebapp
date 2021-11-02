dfb62c8886b4e4dcf248b31b24513d72e4e00627
domo1819 <85992996+domo1819@users.noreply.github.com>
2021-11-02 22:48:59 +0900

    Update index.php

--- index.php
+++ index.php
@@ -46,6 +46,7 @@
 									<optgroup label="検索項目">
 										<option value="all">全件検索</option>
 										<option value="date">日付検索</option>
+										<option value="word">単語検索</option>
 									</optgroup>
 									<optgroup label="地方項目">
 										<option value="つくば">つくば</option>
@@ -72,7 +73,10 @@
 						if(isset($_POST['data']) === TRUE) {
 							$data = $_POST['data'];
 						}
-				                var_dump($data);
+						if(isset($_POST['word']) === TRUE) {
+							$word = $_POST['word'];
+						}
+				    var_dump($data);
 						var_dump($emp);
 						$emp_data = array();
 						$conn = pg_connect(getenv("DATABASE_URL"));
@@ -89,6 +93,9 @@
 							} 
 							if ($emp == 'date'){
 								$result = pg_query($conn, "SELECT a.id, a.timestamp, h.belong_name, c.region_name, b.car_classify_num, b.car_classify_hiragana, b.car_number, a.longitude, a.latitude, e.fine_amount, f.afk_mode, a.is_payment FROM warn_info a INNER JOIN car_data b ON a.car_data_id=b.id INNER JOIN region_data c ON b.car_region_id=c.id INNER JOIN punish_data d ON a.punish_id=d.id INNER JOIN fine_data e ON d.fine_id=e.id INNER JOIN afk_mode_data f ON d.afk_mode_id=f.id INNER JOIN user_data g ON a.user_id=g.user_id INNER JOIN belong_data h ON g.belong_id = h.id WHERE a.timestamp LIKE '%$data%' ");
+							} 
+							if($emp == 'word'){
+								$result = pg_query($conn, "SELECT a.id, a.timestamp, h.belong_name, c.region_name, b.car_classify_num, b.car_classify_hiragana, b.car_number, a.longitude, a.latitude, e.fine_amount, f.afk_mode, a.is_payment FROM warn_info a INNER JOIN car_data b ON a.car_data_id=b.id INNER JOIN region_data c ON b.car_region_id=c.id INNER JOIN punish_data d ON a.punish_id=d.id INNER JOIN fine_data e ON d.fine_id=e.id INNER JOIN afk_mode_data f ON d.afk_mode_id=f.id INNER JOIN user_data g ON a.user_id=g.user_id INNER JOIN belong_data h ON g.belong_id = h.id WHERE a.timestamp LIKE '%$word%' OR h.belong_name LIKE '%$word%' OR c.region_name LIKE '%$word%' OR b.car_classify_hiragana LIKE '%$word%' OR b.car_number LIKE '%$word%' OR f.afk_mode LIKE '%$word%'");
 							} 
 							var_dump($result);
 							
