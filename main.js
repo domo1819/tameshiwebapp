b37cf289677efbe0d9eb65da5aea1a63ba7f56c5
domo1819 <85992996+domo1819@users.noreply.github.com>
2021-11-04 23:01:05 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-$(function() {
-    $('submit').click(function(){
-        $('table').show();
-    });
+$("[emp]").change(function () {
+    // 選択されているvalue属性値を取り出す
+    let val = $("[emp]").val();
+    if (val == date) {
+        //input1を表示
+        $('input[name="data"]').show();
+        //input2を非表示
+        $('input[name="word"]').hide();
+    } else if (val == word) {
+        //input1を非表示
+        $('input[name="data"]').hide();
+        //input2を表示
+        $('input[name="word"]').show();
+    }
 });
