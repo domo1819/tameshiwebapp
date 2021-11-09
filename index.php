3dd851368a1346b5ac3677508fd9abcade3c5172
domo1819 <85992996+domo1819@users.noreply.github.com>
2021-11-09 18:59:15 +0900

    Update index.php

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
 				$result = pg_fetch_all($sql);
 		//④ログイン認証ができたときの処理
 				if ($result[0] != 0){
-					echo '<input type="button" value="ボタン1" onclick="btn_ex2" />';
+					echo '<input type="button" value="ボタン1"  onclick="clickBtn1()">';
 				exit;
 		//⑤アカウント情報が間違っていたときの処理
 				}else{
@@ -59,13 +59,22 @@
 		}
 		?>
 		<script>
-						$(function(){
-								$('.btn_ex2').click(function(){
-										$('main').show();
-								});
-						});
-						</script>
-		<div class="wrapper" id="p1" style="display:none">
+//初期表示は非表示
+document.getElementById("p1").style.display ="none";
+
+function clickBtn1(){
+	const p1 = document.getElementById("p1");
+
+	if(p1.style.display=="block"){
+		// noneで非表示
+		p1.style.display ="none";
+	}else{
+		// blockで表示
+		p1.style.display ="block";
+	}
+}
+</script>
+		<div class="wrapper" id="p1">
 			<header>
 					<?php
 					header("Cache-Control:no-cache,no-store,must-revalidate,max-age=0");
