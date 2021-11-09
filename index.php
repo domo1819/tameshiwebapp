c454a370fb891d39eb9b352c70107fb3692fa284
domo1819 <85992996+domo1819@users.noreply.github.com>
2021-11-09 20:34:00 +0900

    Update index.php

--- index.php
+++ index.php
@@ -117,11 +117,10 @@
 									<input type="text" id="search_text" name="word" placeholder="検索語を入力してください" style="display:none">
 									<br><br><br>
 									<div class="engine">
+							<input type="submit"  name="submit" value="検索" id="submit" style="width:10%;padding:10px;font-size:20px; background-color:#00c4ff; color:#FFF; margin-bottom:10px; margin-left: 15px;">
 							</div>
 						</div>
 					</form>
-					<input type="submit"  name="submit" value="検索" id="submit" style="width:10%;padding:10px;font-size:20px; background-color:#00c4ff; color:#FFF; margin-bottom:10px; margin-left: 15px;">
-							</div>
 				</div>
 				<?php
 						$emp = '';
