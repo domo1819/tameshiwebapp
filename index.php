25fc3bf4ec2c6aebfc270c66735ed347a739e0a2
domo1819 <85992996+domo1819@users.noreply.github.com>
2021-11-09 18:21:41 +0900

    up

--- index.php
+++ index.php
@@ -12,6 +12,52 @@
 		<link rel="stylesheet" href="../css/style.css">
 	</head>
 	<body><!--  人間が見る内容を記述 -->
+	<p>ログイン機能</p>
+	 <div class="sign-up">
+	   <form action="" method="POST">
+				<div>
+				<label for="signup-id">アカウント名</label>
+					<div>
+						<input name="username" id="signup-id" placeholder="IDを入力してください">
+					</div>
+				</div>
+				<div>
+					<label for="signup-pass">パスワード</label>
+						<div>
+							<input name="password" id="signup-pass" placeholder="パスワードを入力してください">
+						</div>
+				</div>
+				<div>
+				<button name="signup" type="submit">Submit</button>
+				</div>
+       </form>
+    </div>	
+		<?php
+		 $err_msg = "";
+
+		 if (isset($_POST['login'])) {
+			$username = $_POST['user_name'];
+			$password = $_POST['password'];
+		//③データが渡ってきた場合の処理
+			try {
+				$conn = pg_connect(getenv("DATABASE_URL"));
+				$sql = pg_query($conn, "SELECT user_name, password FROM user_data where username=? and password=?");
+				$result = pg_fetch_all($sql);
+		//④ログイン認証ができたときの処理
+				if ($result[0] != 0){
+					header('Location: index.php');
+				exit;
+		//⑤アカウント情報が間違っていたときの処理
+				}else{
+					$err_msg = "アカウント情報が間違っています。";
+				}
+		//⑥データが渡って来なかったときの処理
+			}catch (PDOExeption $e) {
+				echo $e->getMessage();
+				exit;
+			}
+		}
+		?>
 		<div class="wrapper">
 			<header>
 					<?php
