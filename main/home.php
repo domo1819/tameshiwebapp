634573d03f26999d8085b25957944da990162b66
domo1819 <85992996+domo1819@users.noreply.github.com>
2021-11-14 22:01:32 +0900

    Update home.php

--- main/home.php
+++ main/home.php
@@ -194,7 +194,7 @@
 									<input type="text" id="search_text" name="word" placeholder="検索語を入力してください" style="display:none">
 									<br><br><br>
 									<div class="engine">
-							<input type="submit"  name="submit" value="検索" id="submit" class="btn btn-outline-info" style="width:10%;padding:10px;font-size:18px;   margin-bottom:10px; margin-left:15px;">
+							<input type="submit"  name="submit" value="検索" id="submit" class="btn btn-outline-info" style="width:10%;padding:10px;font-size:18px;   margin-bottom:10px; margin-left:15px; text-align: center;">
 							</div>
 						</div>
 					</form>
@@ -261,7 +261,7 @@
 							}
 					?>
 					<p>検索結果を表示するには「表示」を押してください</p>
-					<input type="button" id="btn_ex11" class="btn btn-outline-info btn-lg" value="表示"  style="text-align: center; margin-left: 15px; font-size:18px;">
+					<input type="button" id="btn_ex11" class="btn btn-outline-info btn-lg" value="表示"  style="text-align: center; margin-left: 15px; font-size:18px; padding:10px; margin-bottom:10px;">
 					<script>
 						$(function(){
 								$('#btn_ex11').click(function(){
