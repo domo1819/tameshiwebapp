64a6b12cc0b9add939839a2a5c5bf2730fa83e11
domo1819 <85992996+domo1819@users.noreply.github.com>
2021-11-16 12:29:48 +0900

    Update home.php

--- main/home.php
+++ main/home.php
@@ -263,10 +263,12 @@
 						<input type="button" id="btn_ex11" class="btn btn-outline-info btn-lg" value="表示"  style="text-align: center; margin-left: 15px; font-size:18px; padding:10px; margin-bottom:10px;">
 						<script>
 							$(function(){
+								if($row != null){
 									$('#btn_ex11').click(function(){
 											$('table').toggle();
 											$('.none').show();
 									});
+								}
 							});
 							</script>
 						<div class="table-responsive">
