9babdc26be7d117715734a1b97a58106e9d35f81
domo1819 <85992996+domo1819@users.noreply.github.com>
2021-11-16 12:53:11 +0900

    Update home.php

--- main/home.php
+++ main/home.php
@@ -263,8 +263,9 @@
 						<input type="button" id="btn_ex11" class="btn btn-outline-info btn-lg" value="表示"  style="text-align: center; margin-left: 15px; font-size:18px; padding:10px; margin-bottom:10px;">
 						<script>
 							$(function(){
+								var result = $result;
 									$('#btn_ex11').click(function(){
-											if($result !== 0){
+											if(result !== 0){
 												$('table').toggle();
 											}
 											else{
